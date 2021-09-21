b8917d3e8ec51b4975d1605d13cd07487e903fe9
star20001 <72740828+star20001@users.noreply.github.com>
2021-09-21 20:04:01 +0800

    Add files via upload

---
+++ hw02/hw02/Linable.java
@@ -0,0 +1,12 @@
+package hw02;
+
+import hw02.Line.Position;
+
+public interface Linable {
+    public void setPosition(Position position);
+
+    public Position getPosition();
+
+    public int getValue();
+    
+}
